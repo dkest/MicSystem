61f364d4068db8e6c539ed7347d8a5385522b533
dkest <ke.ding@foxmail.com>
2019-07-24 10:43:11 +0800

    分店增加关键字查询

--- Mic.Repository/Api/StoreApiRepository.cs
+++ Mic.Repository/Api/StoreApiRepository.cs
@@ -98,12 +98,14 @@ ContactsPhone='{storeInfo.ContactsPhone}' where UserId={storeInfo.Id};");
                 return Tuple.Create(false, string.Empty, 0, new List<SonStoreInfoParam>());
             }
 
+            string likeSql = string.IsNullOrWhiteSpace(pageParam.Keyword) ? string.Empty : $@" and  b.StoreName like '%{pageParam.Keyword}%')";
+
             string sql = string.Format(@"
                 select top {0} * from (select row_number() over(order by  b.CreateTime desc) as rownumber,
  a.Id,b.StoreName,a.Phone,a.Password,b.Enabled from [User] a left join StoreDetailInfo b on a.Id= b.UserId 
-where a.StoreCode='{2}' and a.UserType=3) temp_row
-                    where temp_row.rownumber>(({1}-1)*{0});", pageParam.PageSize, pageParam.PageIndex, user.StoreCode);
-            int count = Convert.ToInt32(helper.QueryScalar($@"select Count(1) from [User] where Status=1 and UserType=3 and StoreCode= '{user.StoreCode}'  "));
+where a.StoreCode='{2}' and a.UserType=3 {3}) temp_row
+                    where temp_row.rownumber>(({1}-1)*{0});", pageParam.PageSize, pageParam.PageIndex, user.StoreCode,likeSql);
+            int count = Convert.ToInt32(helper.QueryScalar($@"select Count(1) from [User] a left join StoreDetailInfo b on a.Id= b.UserId  where a.Status=1 and a.UserType=3 and a.StoreCode= '{user.StoreCode}'  {likeSql} "));
 
 
 
