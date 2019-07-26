e7da36bff89c5e0fba7e7bf264f9f4ab6f3dcf15
dkest <ke.ding@foxmail.com>
2019-07-26 14:16:17 +0800

    音乐人消息通知模块接口完成

---
+++ Mic.Api/Controllers/SysNoticeController.cs
@@ -0,0 +1,125 @@
+﻿using Mic.Api.Filter;
+using Mic.Api.Models;
+using Mic.Entity;
+using Mic.Repository;
+using Mic.Repository.Api;
+using System.Web.Http;
+
+namespace Mic.Api.Controllers
+{
+    [RoutePrefix("notice")]
+    public class SysNoticeController : ApiController
+    {
+        private NoticeRepository noticeRepository;
+        public SysNoticeController()
+        {
+            noticeRepository = ClassInstance<NoticeRepository>.Instance;
+        }
+
+        /// <summary>
+        /// 根据用户Id，按时间倒序获取系统消息[AUTH]
+        /// </summary>
+        /// <param name="userId">用户Id</param>
+        /// <param name="pageSize">分页大小</param>
+        /// <param name="pageIndex">页码</param>
+        /// <returns></returns>
+        [HttpGet, Route("getNoticeList/{userId:int}/{pageSize:int}/{pageIndex:int}")]
+        [AccessTokenAuthorize]
+        public ResponseResultDto<PagedResult<SysNoticeEntity>> GetNoticeByUserId(int userId, int pageSize, int pageIndex)
+        {
+            var result = noticeRepository.GetNoticeByUserId(userId, pageSize, pageIndex);
+            return new ResponseResultDto<PagedResult<SysNoticeEntity>>
+            {
+                IsSuccess = true,
+                ErrorMessage = string.Empty,
+                Result = new PagedResult<SysNoticeEntity>
+                {
+                    Page = pageIndex,
+                    PageSize = pageSize,
+                    Total = result.Item1,
+                    Results = result.Item2
+                }
+            };
+        }
+
+        /// <summary>
+        /// 根据用户Id，按时间倒序获取未读系统消息[AUTH]
+        /// </summary>
+        /// <param name="userId">用户Id</param>
+        /// <param name="pageSize">分页大小</param>
+        /// <param name="pageIndex">页码</param>
+        /// <returns></returns>
+        [HttpGet, Route("getUnReadNoticeList/{userId:int}/{pageSize:int}/{pageIndex:int}")]
+        [AccessTokenAuthorize]
+        public ResponseResultDto<PagedResult<SysNoticeEntity>> GetUnReadNoticeByUserId(int userId, int pageSize, int pageIndex)
+        {
+            var result = noticeRepository.GetUnReadNoticeByUserId(userId, pageSize, pageIndex);
+            return new ResponseResultDto<PagedResult<SysNoticeEntity>>
+            {
+                IsSuccess = true,
+                ErrorMessage = string.Empty,
+                Result = new PagedResult<SysNoticeEntity>
+                {
+                    Page = pageIndex,
+                    PageSize = pageSize,
+                    Total = result.Item1,
+                    Results = result.Item2
+                }
+            };
+        }
+
+        /// <summary>
+        /// 获取消息详情,将消息标记为已读[AUTH]
+        /// </summary>
+        /// <param name="noticeId">消息通知Id</param>
+        /// <returns></returns>
+        [HttpGet, Route("getNoticeDetail/{noticeId:int}")]
+        [AccessTokenAuthorize]
+        public ResponseResultDto<SysNoticeEntity> GetNoticeDetail(int noticeId)
+        {
+            var result = noticeRepository.GetNoticeDetail(noticeId);
+            noticeRepository.MarkRead(noticeId);
+            return new ResponseResultDto<SysNoticeEntity>
+            {
+                IsSuccess = true,
+                ErrorMessage = string.Empty,
+                Result = result
+            };
+        }
+
+        /// <summary>
+        /// 删除消息通知[AUTH]
+        /// </summary>
+        /// <param name="noticeId">消息通知Id</param>
+        /// <returns></returns>
+        [HttpPost, Route("deleteNotice/{noticeId:int}")]
+        [AccessTokenAuthorize]
+        public ResponseResultDto<bool> DeleteNotice(int noticeId)
+        {
+            var result = noticeRepository.DeleteNotice(noticeId);
+            return new ResponseResultDto<bool>
+            {
+                IsSuccess = true,
+                ErrorMessage = string.Empty,
+                Result = result
+            };
+        }
+        /// <summary>
+        /// 标记为已读[AUTH]
+        /// </summary>
+        /// <param name="noticeId">消息通知Id</param>
+        /// <returns></returns>
+        [HttpPost, Route("markRead/{noticeId:int}")]
+        [AccessTokenAuthorize]
+        public ResponseResultDto<bool> MarkRead(int noticeId)
+        {
+            var result = noticeRepository.MarkRead(noticeId);
+            return new ResponseResultDto<bool>
+            {
+                IsSuccess = true,
+                ErrorMessage = string.Empty,
+                Result = result
+            };
+        }
+    }
+}
---
+++ Mic.Repository/Api/NoticeRepository.cs
@@ -0,0 +1,82 @@
+﻿using Mic.Entity;
+using Mic.Repository.Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Mic.Repository.Api
+{
+    public class NoticeRepository
+    {
+        DapperHelper<SqlConnection> helper;
+        public NoticeRepository()
+        {
+            helper = new DapperHelper<SqlConnection>(WebConfig.SqlConnection);
+        }
+
+        /// <summary>
+        /// 根据用户Id，按时间倒序获取系统消息
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public Tuple<int, List<SysNoticeEntity>> GetNoticeByUserId(int userId, int pageSize, int pageIndex)
+        {
+
+            string sql = string.Format(@"
+                select top {0} * from (select row_number() over(order by NoticeTime desc) as rownumber, 
+* from SysNotice where UserId={2} and Status=1 ) temp_row
+                    where temp_row.rownumber>(({1}-1)*{0})", pageSize, pageIndex, userId);
+            int count = Convert.ToInt32(helper.QueryScalar($@"select count(1) from SysNotice where UserId={userId} and Status=1"));
+
+            return Tuple.Create(count, helper.Query<SysNoticeEntity>(sql).ToList());
+        }
+
+        /// <summary>
+        /// 根据用户Id，按时间倒序获取未读系统消息
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public Tuple<int, List<SysNoticeEntity>> GetUnReadNoticeByUserId(int userId, int pageSize, int pageIndex)
+        {
+            string sql = string.Format(@"
+                select top {0} * from (select row_number() over(order by NoticeTime desc) as rownumber, 
+* from SysNotice where UserId={2} and Status=1 and IsRead=0 ) temp_row
+                    where temp_row.rownumber>(({1}-1)*{0})", pageSize, pageIndex, userId);
+            int count = Convert.ToInt32(helper.QueryScalar($@"select count(1) from SysNotice where UserId={userId} and Status=1 and IsRead=0"));
+
+            return Tuple.Create(count, helper.Query<SysNoticeEntity>(sql).ToList());
+        }
+
+        /// <summary>
+        /// 获取消息详情
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public SysNoticeEntity GetNoticeDetail(int id)
+        {
+            var result = helper.Query<SysNoticeEntity>($@"select * from SysNotice where Id={id}").FirstOrDefault(); ;
+            return result;
+        }
+
+        /// <summary>
+        /// 删除消息通知
+        /// </summary>
+        /// <param name="noticeId"></param>
+        /// <returns></returns>
+        public bool DeleteNotice(int noticeId)
+        {
+            return helper.Execute($@"update SysNotice set Status=0 where Id={noticeId}") > 0 ? true : false;
+        }
+        /// <summary>
+        /// 标记为已读
+        /// </summary>
+        /// <param name="noticeId"></param>
+        /// <returns></returns>
+        public bool MarkRead(int noticeId)
+        {
+            return helper.Execute($@"update SysNotice set IsRead=1 where Id={noticeId}") > 0 ? true : false;
+        }
+
+    }
+}
